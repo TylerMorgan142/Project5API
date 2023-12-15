56f80fb959ed3466ceabeba84b872f16bd048a61
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-15 05:42:57 +0000

    Added comments model and serializer

--- comments/models.py
+++ comments/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from posts.models import Post
 
-class Post(models.Model):
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Post
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
+    content = models.TextField()
 
     class Meta:
         ordering = ['-created_at']
-    
+
     def __str__(self):
-        return f'{self.id} {self.title}'
+        return self.content
