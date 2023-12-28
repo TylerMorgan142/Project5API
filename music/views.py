4d0b448431c249839f8e1aec64f2cd9ac40dc857
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-28 22:18:13 +0000

    Removed references to genre and song models

--- music/views.py
+++ music/views.py
@@ -1,10 +1,8 @@
 from rest_framework import generics
-from .models import Genre, Artist, Album, Song
-from .serializers import GenreSerializer, ArtistSerializer, AlbumSerializer, SongSerializer
+from .models import Artist, Album
+from .serializers import  ArtistSerializer, AlbumSerializer
 
-class GenreListView(generics.ListAPIView):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
+
 
 class ArtistListView(generics.ListAPIView):
     queryset = Artist.objects.all()
@@ -14,6 +12,3 @@ class AlbumListView(generics.ListAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
-class SongListView(generics.ListAPIView):
-    queryset = Song.objects.all()
-    serializer_class = SongSerializer
