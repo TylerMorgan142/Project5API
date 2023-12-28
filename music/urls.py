268d7231b2a31645b7c5f912b389aefb76762033
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-28 22:27:37 +0000

    Added artist and album create views

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from music.views import  ArtistListView, AlbumListView
+from music.views import  ArtistListView, AlbumListView, ArtistCreateView, AlbumCreateView
 
 
 urlpatterns = [
     path('artists/', ArtistListView.as_view(), name='artist-list'),
     path('albums/', AlbumListView.as_view(), name='album-list'),
+    path('artists/create/', ArtistCreateView.as_view(), name='create_artist'),
+    path('albums/create/', AlbumCreateView.as_view(), name='create_album'),
 ]
--- music/views.py
+++ music/views.py
@@ -12,3 +12,10 @@ class AlbumListView(generics.ListAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
+class ArtistCreateView(generics.CreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class AlbumCreateView(generics.CreateAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
