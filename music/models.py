614a7c96c906bc9508d5ec462df14b907742ab8f
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-29 03:12:42 +0000

    removed fields from Album model

--- music/models.py
+++ music/models.py
@@ -5,15 +5,11 @@ from django.apps import apps
 
 class Artist(models.Model):
     name = models.CharField(max_length=255)
-    
-
     def __str__(self):
         return self.name
 
 class Album(models.Model):
     title = models.CharField(max_length=255)
-    release_date = models.DateField(null=True, blank=True)
-    artist = models.ForeignKey('music.Artist', on_delete=models.CASCADE, related_name='albums')
 
     def __str__(self):
         return f"{self.artist.name} - {self.title}"
