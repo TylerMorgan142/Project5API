e145bd91d8abfd93a2d62f5885f2c2e6d64c4e50
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-29 03:18:42 +0000

    fixed typo

--- Metalhub_api/settings.py
+++ Metalhub_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEV' in os.environ
-DEVBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
