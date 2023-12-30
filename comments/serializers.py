e013dc92ef1a79dfe37fe0e4c060a5d34b42d12d
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-30 00:49:33 +0000

    Epanded Comment serializer to differentiate between posts and reviews

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,16 +4,14 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Comment model
-    Adds three extra fields when returning a list of Comment instances
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
+    post = serializers.SerializerMethodField()
+    review = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -25,12 +23,23 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
 
+    def get_post(self, obj):
+        if obj.post:
+            return {'id': obj.post.id, 'title': obj.post.title}
+        return None
+
+    def get_review(self, obj):
+        if obj.review:
+            return {'id': obj.review.id, 'title': obj.review.title}
+        return None
+
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content'
+            'post', 'review', 'created_at', 'updated_at', 'content'
         ]
+
 
 
 class CommentDetailSerializer(CommentSerializer):
