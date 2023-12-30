78b12556bfd3a88e8926116630ca3f2ed72e3dd1
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-30 03:17:27 +0000

    Added get_post and get_review methods to comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,11 @@
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
+from music.models import Review
+from posts.models import Post
+from music.serializers import AlbumSerializer
+from posts.serializers import PostSerializer
+from django.contrib.auth.models import User
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -23,18 +28,32 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
 
+    def get_post(self, obj):
+        if obj.post:
+            post_data = {
+                'id': obj.post.id,
+                'title': obj.post.title,
+                'content': obj.post.content,
+            }
+            return PostSerializer(post_data).data
+        return None
+
+    def get_review(self, obj):
+        if obj.review:
+            review_data = {
+                'id': obj.review.id,
+                'owner': obj.review.owner.username,
+                'album': AlbumSerializer(obj.review.album).data,
+                'rating': obj.review.rating,
+                'title': obj.review.title,
+                'content': obj.review.content,
+            }
+            return review_data
+        return None
+
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'review', 'created_at', 'updated_at', 'content'
         ]
-
-
-
-class CommentDetailSerializer(CommentSerializer):
-    """
-    Serializer for the Comment model used in Detail view
-    Post is a read only field so that we dont have to set it on each update
-    """
-    post = serializers.ReadOnlyField(source='post.id')
