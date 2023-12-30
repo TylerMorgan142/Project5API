a5ef809bd75e0a0b077adb762fb476d24a689e43
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-30 07:17:46 +0000

    removed reviews/create url

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,6 @@ from music.views import AlbumListView, AlbumCreateView, ReviewList, ReviewDetail
 urlpatterns = [
     path('reviews/', ReviewList.as_view(), name='review-list'),
     path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
-    path('reviews/create/', ReviewList.as_view(), name='review-create'),
     path('albums/', AlbumListView.as_view(), name='album-list'),
     path('albums/create/', AlbumCreateView.as_view(), name='create_album'),
 ]
