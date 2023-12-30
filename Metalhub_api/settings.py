9713ef009c5c651b6b34334c3af6ceb410be9a6b
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-30 06:10:45 +0000

    Added react app to CORS

--- Metalhub_api/settings.py
+++ Metalhub_api/settings.py
@@ -121,7 +121,7 @@ MIDDLEWARE = [
 #     ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://metalhub-api-7e3be8a93e64.herokuapp.com",
+    "https://metalhub-api-7e3be8a93e64.herokuapp.com", "https://metalhub-5230d8c858ac.herokuapp.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
