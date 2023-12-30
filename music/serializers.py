7529c9586303a9c0eb6bb0dc7f868c4d779fa0fd
TylerMorgan142 <tylermorgan142@gmail.com>
2023-12-30 01:00:45 +0000

    removed likes and comments from reviews

--- music/serializers.py
+++ music/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Album, Review
-from likes.models import Like
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -8,31 +7,20 @@ class ReviewSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    like_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
 
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
     
-    def get_like_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, review=obj
-            ).first()
-            return like.id if like else None
-        return None
+    
 
     class Meta:
         model = Review
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content','like_id', 'likes_count', 
-            'comments_count', 'rating'
+            'title', 'content', 'rating'
         ]
 
 class AlbumSerializer(serializers.ModelSerializer):
--- music/views.py
+++ music/views.py
@@ -10,8 +10,6 @@ class ReviewList(generics.ListCreateAPIView):
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Review.objects.annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
@@ -20,17 +18,11 @@ class ReviewList(generics.ListCreateAPIView):
     ]
     filterset_fields = [
         'owner__followed__owner__profile',
-        'likes__owner__profile',
         'owner__profile',
     ]
     search_fields = [
         'owner__username',
         'title',
-    ]
-    ordering_fields = [
-        'likes_count',
-        'comments_count',
-        'likes__created_at',
     ]
 
     def perform_create(self, serializer):
@@ -40,10 +32,6 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     
     serializer_class = ReviewSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Review.objects.annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
-    ).order_by('-created_at')
 
 
 class AlbumListView(generics.ListAPIView):
