3c83140ef3d5ef3776765b0e5087fce8c8d3e1ab
TylerMorgan142 <tylermorgan142@gmail.com>
2024-01-09 07:29:21 +0000

    Altered comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,23 +3,17 @@ from rest_framework import serializers
 from .models import Comment
 
 
-from django.contrib.humanize.templatetags.humanize import naturaltime
-from rest_framework import serializers
-from .models import Comment
-from posts.models import Post
-from music.serializers import AlbumSerializer
-from posts.serializers import PostSerializer
-from django.contrib.auth.models import User
-
-
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model
+    Adds three extra fields when returning a list of Comment instances
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
-    post = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -31,23 +25,12 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
 
-    def get_post(self, obj):
-        if obj.post:
-            post_data = {
-                'id': obj.post.id,
-                'title': obj.post.title,
-                'content': obj.post.content,
-            }
-            return PostSerializer(post_data).data
-        return None
-
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
-
 
 
 class CommentDetailSerializer(CommentSerializer):
